bc66f12cfe772d26e5bf5003e5337a7931a26a05
Gotlibbb <gleb.evgenevichh@gmail.com>
2021-06-05 18:53:30 +0300

    Updates

---
+++ precache-manifest.e5bb547093cf60f3f089cb626255b972.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5213cc231afee1c8baf4fa69ca301590",
+    "url": "/ReactGotlibbbWay/index.html"
+  },
+  {
+    "revision": "8b86c83265e7e9074dca",
+    "url": "/ReactGotlibbbWay/static/css/main.edfbc6c9.chunk.css"
+  },
+  {
+    "revision": "ae417cf161cab554f886",
+    "url": "/ReactGotlibbbWay/static/js/2.129db669.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/ReactGotlibbbWay/static/js/2.129db669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b86c83265e7e9074dca",
+    "url": "/ReactGotlibbbWay/static/js/main.11677f46.chunk.js"
+  },
+  {
+    "revision": "931d21572bc448ae92fa",
+    "url": "/ReactGotlibbbWay/static/js/runtime-main.de0ff6a7.js"
+  },
+  {
+    "revision": "27d2bb2ee07169abfb80311bec23bae4",
+    "url": "/ReactGotlibbbWay/static/media/dialog.27d2bb2e.svg"
+  },
+  {
+    "revision": "4a8937d89a1bdd7612acb7f883b61db5",
+    "url": "/ReactGotlibbbWay/static/media/logout.4a8937d8.svg"
+  },
+  {
+    "revision": "b07ea12eeda34e935ce83f3352044a7c",
+    "url": "/ReactGotlibbbWay/static/media/my_ava.b07ea12e.jpg"
+  },
+  {
+    "revision": "6c9609f2f9fc1e736615cc8a008630bb",
+    "url": "/ReactGotlibbbWay/static/media/no_ava.6c9609f2.png"
+  },
+  {
+    "revision": "35d5736d8dabee9268eb09d5624d7f68",
+    "url": "/ReactGotlibbbWay/static/media/preloader.35d5736d.gif"
+  },
+  {
+    "revision": "7de5e5f7d3fbd734c8d9328cc6806365",
+    "url": "/ReactGotlibbbWay/static/media/user.7de5e5f7.svg"
+  },
+  {
+    "revision": "08be9bcef3a1f0b6820d523f59fe2a52",
+    "url": "/ReactGotlibbbWay/static/media/users.08be9bce.svg"
+  }
+]);
